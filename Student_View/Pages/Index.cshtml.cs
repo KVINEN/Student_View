084de27fa0654682b5341101c8b98e928e874a88
KVINEN <benjamin.roiland@hotmail.no>
2025-04-07 13:30:56 +0200

    soking paa navn

--- Student_View/Pages/Index.cshtml.cs
+++ Student_View/Pages/Index.cshtml.cs
@@ -1,20 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Student_View.Entity;
 
 namespace Student_View.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly Context _context;
+        public IndexModel(Context context)
         {
-            _logger = logger;
+            _context = context;
         }
+        public List<Student> AllStudents { get; set; } = new();
 
+        [BindProperty (SupportsGet = true)]
+        public string? Sok { get; set; }
         public void OnGet()
         {
-
+            if(!string.IsNullOrEmpty(Sok))
+            {
+                AllStudents = _context.Students
+                    .Where(s => s.Studentname.Contains(Sok))
+                    .Include(s => s.Grades)
+                    .ToList();
+            }
+            else
+            {
+                AllStudents = _context.Students
+                    .Include(s => s.Grades)
+                    .ToList();
+            }
         }
     }
 }
