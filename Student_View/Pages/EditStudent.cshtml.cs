1e283cf64a92914b0f6c722a8e2e732c0c729994
KVINEN <benjamin.roiland@hotmail.no>
2025-04-10 13:12:50 +0200

    edit student

---
+++ Student_View/Pages/EditStudent.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Student_View.Pages
+{
+    public class EditStudentModel : PageModel
+    {
+        public void OnPost()
+        {
+
+        }
+    }
+}
